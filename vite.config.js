74a4b705ed44052ca09edad4d532e63f42c10c71
Hussein Fayek <hussein.fayek7@gmail.com>
2025-09-10 01:21:20 +0300

    1

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,9 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-export default defineConfig({ plugins: [vue()] })
+export default defineConfig({
+  plugins: [vue()],
+  build: {
+    outDir: 'dist'
+  }
+})
